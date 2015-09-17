d56362a240457c067cb7ca32a392ada6433bc8e6
dannyvassallo <danielvassallo87@gmail.com>
2015-09-17 12:29:46 -0400

    added timestamps to CSV files and pin entries in them

--- app/controllers/pinmanager_controller.rb
+++ app/controllers/pinmanager_controller.rb
@@ -8,7 +8,7 @@ class PinmanagerController < ApplicationController
       if params[:search]
         @pins = Pin.search(params[:search]).order("created_at DESC").paginate(:page => params[:page], :per_page => result_num)
       else
-        @posts = Pin.all.order('created_at DESC').paginate(:page => params[:page], :per_page => result_num)
+        @pins = Pin.all.order('created_at DESC').paginate(:page => params[:page], :per_page => result_num)
       end
     else
       redirect_to root_path
