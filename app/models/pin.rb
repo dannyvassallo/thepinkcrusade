44f7cb54ec1d5f274224a132d2908a29459a8590
dannyvassallo <danielvassallo87@gmail.com>
2015-09-18 14:59:22 -0400

    profanity filter

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,6 +5,7 @@ class Pin < ActiveRecord::Base
   validates_presence_of :pin_comment
   validates_presence_of :pin_zip
   validates_presence_of :pin_app_name
+  profanity_filter :pin_comment, :pin_first_name, :pin_last_name
 
   def self.search(search)
     Pin.where("lower(pin_first_name || ' ' || pin_last_name || ' ' || pin_comment || ' ' || pin_email || ' ' || pin_app_name) LIKE ?", "%#{search.downcase}%")
