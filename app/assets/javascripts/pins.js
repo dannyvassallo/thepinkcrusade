7bd0322986000d1194975357f47d5c32744830a1
dannyvassallo <danielvassallo87@gmail.com>
2015-09-21 12:52:50 -0400

    added grey background to HTML5 datepicker on iOS

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -6,9 +6,36 @@
 // 	if(window.height() )
 // 	$('#the-wrap').css('min-height', window.innerHeight);
 // });
+function iOS() {
 
-$(function(){
+  var iDevices = [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ];
+
+  while (iDevices.length) {
+    if (navigator.platform === iDevices.pop()){ return true; }
+  }
+
+  return false;
+}
+
+
+$(function(){	
 	if ( $('.datepicker').prop('type') != 'date' ) {
 	    $('.datepicker').datepicker();
-	}
+	};
+	$('#info').on('shown', function () {
+		// alert('open')
+		if (iOS()){
+			$('.datepicker').css('background-color','#ccc !important');
+			// alert('true');
+		} else {
+			// alert('false');
+		}
+	});
 });
