bbe6ec80d77876040c0a39542b1e5597e1ab21a8
dannyvassallo <danielvassallo87@gmail.com>
2015-09-21 12:13:43 -0400

    added missing class to JS file

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -9,6 +9,6 @@
 
 if (datefield.type!="date"){ //if browser doesn't support input type="date", initialize date picker widget:
    jQuery(function($){ //on document.ready
-       $().datepicker();
+       $('.datepicker').datepicker();
    })
 }
